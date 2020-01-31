945a8cf6cdfd51d2ee556a7e17ebf3fa4f1d13c7
VMoskalev <vladislav.moskalev@gmail.com>
2020-01-31 19:41:22 +0300

    change filter names in view, combine get/post methods and renamed them

--- src/main/java/ru/homecredit/microservice/web/DashboardController.java
+++ src/main/java/ru/homecredit/microservice/web/DashboardController.java
@@ -18,21 +18,13 @@ public class DashboardController {
     private final IMicroserviceDaoService microserviceDaoService;
     private final AutomaticCheckHealth automaticCheckHealth;
 
-    @RequestMapping(value = "/envs", method = RequestMethod.GET)
-    public String getEnvList(Model model) {
-        List<String> distinctDescs = microserviceDaoService.getDistinctEnvDescs();
-
-        model.addAttribute("envDescList", distinctDescs);
-        return "environment_list";
-    }
-
-    @RequestMapping(value = "/envs", method = RequestMethod.POST)
-    public String getEnvList(@RequestParam String filter, Model model) {
+    @RequestMapping(value = "/envs", method = {RequestMethod.GET, RequestMethod.POST})
+    public String getEnvList(@RequestParam(required = false) String envFilter, Model model) {
         List<String> list;
-        if (filter != null && !filter.isEmpty()) {
+        if (envFilter != null && !envFilter.isEmpty()) {
             list = microserviceDaoService.getDistinctEnvDescs().stream()
                     // в слове из поиска заменяем пробелы на +, шобы искалось хорошо
-                    .filter(m -> m.toLowerCase().contains(filter.toLowerCase().replaceAll("\\s+", "+")))
+                    .filter(m -> m.toLowerCase().contains(envFilter.toLowerCase().replaceAll("\\s+", "+")))
                     .collect(Collectors.toList());
         } else {
             list = microserviceDaoService.getDistinctEnvDescs();
@@ -42,36 +34,29 @@ public class DashboardController {
         return "environment_list";
     }
 
-    @RequestMapping(value = "/envs/{env}", method = RequestMethod.GET)
-    public String getServiceListByEnv(@PathVariable String env, Model model) {
-        List<Microservice> serviceNames = microserviceDaoService.getServicesByEnvDesc(env);
-
-        model.addAttribute("env", env);
-        model.addAttribute("serviceList", serviceNames);
-        return "service_list";
-    }
-
-    @RequestMapping(value = "/envs/{env}", method = RequestMethod.POST)
-    public String postServiceListByEnv(@PathVariable String env, String filter, Model model) {
+    @RequestMapping(value = "/envs/{env}", method = {RequestMethod.GET, RequestMethod.POST})
+    public String getServiceListByEnv(@PathVariable("env") String envDesc,
+                                      @RequestParam(required = false) String serviceFilter,
+                                      Model model) {
         List<Microservice> serviceNames;
-        if (filter != null && !filter.isEmpty()) {
-            serviceNames = microserviceDaoService.getServicesByEnvDesc(env).stream()
-                    .filter(m -> m.getServiceName().toLowerCase().contains(filter.toLowerCase().replaceAll("\\s+", "+")))
+        if (serviceFilter != null && !serviceFilter.isEmpty()) {
+            serviceNames = microserviceDaoService.getServicesByEnvDesc(envDesc).stream()
+                    .filter(m -> m.getServiceName().toLowerCase().contains(serviceFilter.toLowerCase().replaceAll("\\s+", "+")))
                     .collect(Collectors.toList());
         } else {
-            serviceNames = microserviceDaoService.getServicesByEnvDesc(env);
+            serviceNames = microserviceDaoService.getServicesByEnvDesc(envDesc);
         }
 
-        model.addAttribute("env", env);
+        model.addAttribute("env", envDesc);
         model.addAttribute("serviceList", serviceNames);
         return "service_list";
     }
 
     @RequestMapping(value = "/{env}/{service}", method = RequestMethod.GET)
-    public String getSmth1(@PathVariable String env,
-                           @PathVariable String service,
-                           Model model) {
-        String envName = microserviceDaoService.getServicesByEnvDesc(env).stream()
+    public String getPossibleActuatorMethods(@PathVariable("env") String envDesc,
+                                             @PathVariable String service,
+                                             Model model) {
+        String envName = microserviceDaoService.getServicesByEnvDesc(envDesc).stream()
                 .filter(microservice -> microservice.getServiceName().equalsIgnoreCase(service))
                 .collect(Collectors.toList()).get(0).getEnvName();
 
@@ -80,14 +65,60 @@ public class DashboardController {
         return "in_service";
     }
 
-    @RequestMapping(value = "/{envName}/{service}/{method}", method = RequestMethod.GET)
+    @RequestMapping(value = "/{env}/{service}/{method}", method = RequestMethod.GET)
     @ResponseBody
-    public String getActuatorsMethods(@PathVariable String envName,
-                                       @PathVariable String service,
-                                       @PathVariable String method) {
+    public String getRequestFromService(@PathVariable("env") String envName,
+                                        @PathVariable String service,
+                                        @PathVariable String method) {
         return automaticCheckHealth.getRequestFromCache(envName, service, method);
     }
+
+//    "/envs?env=fp&service=transfer"
+//    @RequestMapping(value = "/envs2", method = {RequestMethod.GET, RequestMethod.POST})
+//    public String what(@RequestParam(required = false) String envFilter,
+//                       @RequestParam(required = false) String envDesc,
+//                       @RequestParam(required = false) String serviceFilter,
+//                       @RequestParam(required = false) String service,
+//                       Model model) {
+//        List<String> list;
+//        if (envFilter != null && !envFilter.isEmpty()) {
+//            list = microserviceDaoService.getDistinctEnvDescs().stream()
+//                    // в слове из поиска заменяем пробелы на +, шобы искалось хорошо
+//                    .filter(m -> m.toLowerCase().contains(envFilter.toLowerCase().replaceAll("\\s+", "+")))
+//                    .collect(Collectors.toList());
+//        } else {
+//            list = microserviceDaoService.getDistinctEnvDescs();
+//        }
 //
+//        model.addAttribute("envDescList", list);
+//
+//        if (envDesc == null)
+//            return "environment_list2";
+//
+//        List<Microservice> serviceNames;
+//        if (serviceFilter != null && !serviceFilter.isEmpty()) {
+//            serviceNames = microserviceDaoService.getServicesByEnvDesc(envDesc).stream()
+//                    .filter(m -> m.getServiceName().toLowerCase().contains(serviceFilter.toLowerCase().replaceAll("\\s+", "+")))
+//                    .collect(Collectors.toList());
+//        } else {
+//            serviceNames = microserviceDaoService.getServicesByEnvDesc(envDesc);
+//        }
+//
+//        model.addAttribute("env", envDesc);
+//        model.addAttribute("serviceList", serviceNames);
+//
+//        if (service == null)
+//            return "service_list2";
+//
+//        String envName = microserviceDaoService.getServicesByEnvDesc(envDesc).stream()
+//                .filter(microservice -> microservice.getServiceName().equalsIgnoreCase(service))
+//                .collect(Collectors.toList()).get(0).getEnvName();
+//
+//        model.addAttribute("envName", envName);
+//        model.addAttribute("service", service);
+//        return "in_service2";
+//    }
+
 //    @GetMapping("/x")
 //    @ResponseBody
 //    public void whaat1() {
